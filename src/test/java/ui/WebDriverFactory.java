f146ce83615e0ed2dd6d4f5dd3e6d8fd1f21110a
Maxula0234 <max0234@icloud.com>
2021-09-26 21:22:42 +0300

    - add factory -add enum

---
+++ src/test/java/ui/WebDriverFactory.java
@@ -0,0 +1,20 @@
+package ui;
+
+import Constant.WebDriverName;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebDriverFactory {
+
+    public static WebDriver getWebDriver(WebDriverName webDriverName) {
+        switch (webDriverName) {
+            case CHROME: {
+                return new ChromeDriver();
+            }
+            case SAFARI: {
+                break;
+            }
+        }
+        return null;
+    }
+}
